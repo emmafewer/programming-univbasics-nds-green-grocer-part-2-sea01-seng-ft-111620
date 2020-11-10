113e036a5d721ea690f856bd1c5c07add893c060
Emma Fewer <emmafewer20@gmail.com>
2020-11-10 17:59:54 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -42,7 +42,7 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   cart_with_coupons = apply_coupons(consolidated_cart, coupons)
-  cart_with_coupons_clearance = apply_clearance(cart_with_coupons)
+  cart_with_coupons_clearance = apply_clearance(consolidated_cart)
   
   counter = 0 
   while counter < cart_with_coupons_clearance.length do 
