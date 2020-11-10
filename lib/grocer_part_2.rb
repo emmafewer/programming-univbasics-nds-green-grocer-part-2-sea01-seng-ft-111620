54883700a752bb8cf88338c82eb45572ac9028ca
Emma Fewer <emmafewer20@gmail.com>
2020-11-10 17:56:27 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -51,11 +51,4 @@ def checkout(cart, coupons)
   end
   total
   
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
 end
